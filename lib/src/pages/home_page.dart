f67a6a874db04ccac1bcffb20c2281002a9fe018
berseckchr00 <berseckchr00@gmail.com>
2021-05-09 14:35:30 -0400

    test

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Componentes'),
+        title: Text('Componentes list'),
       ),
       body: _createLista(),
     );
